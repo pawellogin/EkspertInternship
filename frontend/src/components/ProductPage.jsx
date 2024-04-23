3005ca499b400bd4305a3a72cff3b0e800742852
pawellogin <pawellogin712@gmail.com>
2024-04-23 14:52:57 +0200

    Add search bar for ProductPage

--- frontend/src/components/ProductPage.jsx
+++ frontend/src/components/ProductPage.jsx
@@ -2,33 +2,67 @@ import React, { useEffect, useState } from 'react'
 import ProductService from "../services/ProductService"
 import AuthHeader from '../services/AuthHeader'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { faArrowLeft, faArrowRight, faSearch } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
-
-
-
-
-
 
 const ProductPage = () => {
     const [products, setProducts] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
     const [productsPerPage, setProductsPerPage] = useState(5);
+    const [searchQuery, setSearchQuery] = useState('')
 
-    const fetchData = async () => {
-        try {
-          const response = await ProductService.GetProductsPage(currentPage - 1, productsPerPage);
-        //   console.log(response.data.content);
-          setProducts(response.data.content);
+    const fetchDataByPage = async () => {
+        try{
+            const response = await ProductService.GetProductsPage(currentPage - 1, productsPerPage);
+            setProducts(response.data.content);   
+            // console.log("fetch page data");            
         } catch (error) {
           console.error('Error fetching products:', error);
         }
     };
 
+    const fetchDataAll = async () => {
+        try{
+            const response = await ProductService.GetProductsPage(0, 0);
+            setProducts(response.data.content);
+            console.log("fetch all data");
+        }catch(error) {
+            console.error("Error fetch all products: ", error);
+        }
+    }
+
+    const fetchDataAllAndFilter = async () => {
+        try{
+            const response = await ProductService.GetProductsPage(0,0);
+            const allProducts = response.data.content;
+            setProducts(allProducts);
+
+            if(searchQuery){
+                setProducts(allProducts.filter(product => product.name.toLowerCase().includes(searchQuery.toLowerCase())));
+            }
+            
+        }catch(error){
+            console.error("Error fetch all products with filter: ", error);
+        }
+    }
+
+    const handleSearchButtonClick = () => {
+        if(searchQuery){
+            fetchDataAllAndFilter();
+        }else {
+            fetchDataByPage();
+        }
+
+    }
+
     const changeToPreviousPage = () => {
         if(currentPage > 1){
             setCurrentPage(currentPage - 1);
         }
+    }
+
+    const handleSearchInputChange = (event) => {
+        setSearchQuery(event.target.value);
     }
 
     const changeToNextPage = () => {
@@ -40,21 +74,21 @@ const ProductPage = () => {
     }
 
     useEffect(() => {
-        fetchData();
+        fetchDataByPage();
+        // fetchDataAll();
     }, [currentPage, productsPerPage]);
 
-    const paginate = (pageNumber) => setCurrentPage(pageNumber);
+    useEffect(() =>{
+        // console.log("Products: ", products)
+    }, [products])
 
-    const indexOfLastProduct = currentPage * productsPerPage;
-    const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
-    const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);
 
     return (
         <section>
             <h2>Product Page</h2>
             <div className="productPage-buttons-container">
                 <Link type="button" className="btn btn-secondary" to="/manageProducts/addNewProduct">Add New Product</Link>
-                
+
                 <div className="dropdown">
                     <button
                         className="btn btn-secondary dropdown-toggle"
@@ -71,6 +105,16 @@ const ProductPage = () => {
                         <button className="dropdown-item" type='button' onClick={() => setProductsPerPage(10)}>10</button>
                         <button className="dropdown-item" type='button' onClick={() => setProductsPerPage(30)}>30</button>
                     </div>
+                </div>
+
+                <div className="search-bar ProductPage-search-bar">
+                    <input
+                        type="text"
+                        placeholder="Search products..."
+                        value={searchQuery}
+                        onChange={handleSearchInputChange}
+                    />
+                    <button type="button" className="btn btn-secondary" onClick={handleSearchButtonClick}><FontAwesomeIcon icon={faSearch} /></button>
                 </div>
             </div>
 
