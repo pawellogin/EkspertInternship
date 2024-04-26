bc68e5cf808eb14664b72fe69dd3cf2aa8dcfd1f
pawellogin <pawellogin712@gmail.com>
2024-04-26 13:06:45 +0200

    Add Navbar exteranl css

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import '../styles/Navbar.css'
 
 import AuthService from '../services/AuthService'; // Import AuthService
 
