a305e9bb5943acb58945986e6b81b98c0f7a4491
pawellogin <pawellogin712@gmail.com>
2024-04-26 13:00:14 +0200

    Add external css for welcome

--- frontend/src/components/Welcome.jsx
+++ frontend/src/components/Welcome.jsx
@@ -2,6 +2,7 @@ import { DemoService } from "../services/DemoService"
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import AuthService from "../services/AuthService";
+import '../styles/Welcome.css'
 
 const Welcome = () => {
 
@@ -10,16 +11,18 @@ const Welcome = () => {
   const isUserLoggedIn = AuthService.checkIsUserLoggedIn();
 
     return (
-      <section className="WelcomePage-section">
+      <section className="welcome">
         {isUserLoggedIn ? (
           <>
-          <p>Welcome</p>
+          <h1 className="welcome-title">Welcome</h1>
           </>
         ) : (
           <>
-          <h1>Welcome, please login or register:</h1>
-          <Link type="button" className="btn btn-secondary" to="/login">Login</Link>
-          <Link type="button" className="btn btn-secondary" to="/register">Register</Link>
+          <h1 className="welcome-title">Welcome, please login or register:</h1>
+          <div className="welcome-links">
+            <Link type="button" className="btn btn-secondary welcome-login-link" to="/login">Login</Link>
+            <Link type="button" className="btn btn-secondary welcome-register-link" to="/register">Register</Link>
+          </div>
           </>
         )}
       </section>
