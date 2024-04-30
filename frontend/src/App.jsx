023fb97e51a9f8ef26b1e35e13d62c7d2bc4ea5f
pawellogin <pawellogin712@gmail.com>
2024-04-30 10:41:32 +0200

    Change routes to check isUserLoggedIn

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import * as React from "react";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Layout from './components/Layout';
 import Welcome from './components/Welcome';
 import AuthService from './services/AuthService';
@@ -23,23 +23,32 @@ function App() {
     console.log("handel logout in app");
   }
 
+  const isUserLoggedIn = AuthService.checkIsUserLoggedIn();
+
   return (
     <Router>
       <div>
-        <Layout onLogout = {handleLogout}>
-          <Routes>
-          <Route exact path="/login" element={<LoginForm />} />
-          <Route exact path="/register" element={<RegisterForm />} />
-          <Route exact path="" element={<Welcome/>}/>
-          <Route exact path="/profile" element={<LoggedUserInformation/>}/>
-          <Route exact path="/manageProducts" element={<ProductListAdmin/>}/>
-          <Route path="manageProducts/:productId" element={<ProductEditAdmin/>}/>;
-          <Route exact path="/manageUsers" element={<UsersPageAdmin/>}/>
-          <Route path="manageUsers/:userId" element={<UserEditAdmin/>}/>
-          <Route exact path="/manageProducts/addNewProduct" element={<AddProductAdmin/>}/>
-          <Route path="products/:productId" element={<ProductListDetails/>}/>
-          <Route exact path="/cart" element={<Cart/>}/>
-          </Routes>
+      <Layout onLogout={handleLogout}>
+          {isUserLoggedIn ? (
+            <Routes>
+              <Route path="/" element={<Welcome />} />
+              <Route path="/profile" element={<LoggedUserInformation />} />
+              <Route path="/manageProducts" element={<ProductListAdmin />} />
+              <Route path="manageProducts/:productId" element={<ProductEditAdmin />} />
+              <Route path="/manageUsers" element={<UsersPageAdmin />} />
+              <Route path="manageUsers/:userId" element={<UserEditAdmin />} />
+              <Route path="/manageProducts/addNewProduct" element={<AddProductAdmin />} />
+              <Route path="products/:productId" element={<ProductListDetails />} />
+              <Route path="/cart" element={<Cart />} />
+            </Routes>
+          ) : (
+            <Routes>
+              <Route path="/" element={<Welcome />} />
+              <Route path="/login" element={<LoginForm />} />
+              <Route path="/register" element={<RegisterForm />} />
+              <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+          )}
         </Layout>
       </div>
     </Router>
